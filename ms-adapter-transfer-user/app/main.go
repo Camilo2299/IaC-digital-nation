cc9d2b3b0d2c4453dbd9ad72ec72d7d32308c9da
Juan Camilo Echeverry Camayo <juan.echeverry@neoris.com>
2024-03-13 17:02:38 -0500

    Prueba para hacer build

--- ms-adapter-transfer-user/app/main.go
+++ ms-adapter-transfer-user/app/main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	u "ms-adapter-transfer-user/domain/model"
-)
+import "fmt"
 
 func main() {
-
-	//documents := []string{"http://fexample.com/document1", "http://example.com/document2"}
-	document := map[string]string{
-		"url1: ": "http://fexample.com/document1",
-		"url2: ": "http://example.com/document2",
-	}
-
-	user := u.User{
-		ID:       1102399932,
-		Nombre:   "Jose Oliva",
-		Email:    "jose.oliva@micarpeta.com",
-		Document: u.Document{document}}
-	//user := u.MiMetodo("hola")
-
-	fmt.Println(user.Document)
+	fmt.Println("Hello, World!")
 }
